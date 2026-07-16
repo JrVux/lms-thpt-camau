a539d72f97b1fc2d1b27a9d7c056d1eaf79b89b2
admin <admin@lms.local>
2026-07-16 19:23:00 +0700

    Simplify build with postinstall

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Installing frontend dependencies..."
-cd frontend && npm install
-echo "Building frontend..."
-npm run build
-echo "Installing backend dependencies..."
-cd ../backend && npm install
-echo "Build complete!"
